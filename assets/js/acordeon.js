e42dd3d8f3b4bb0b394da672bb32488cb1ac7549
lilianayumi <lilianayumi97@gmail.com>
2023-05-16 22:41:27 -0300

    upload

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,15 +1,16 @@
-
-const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
-
+const acordeonTriggers = document.querySelectorAll('.acordeon .trigger');
 acordeonTriggers.forEach((trigger) => {
     trigger.addEventListener('click', (e) => {
-        const acordeon = trigger.parentElement
-        const isOpen = acordeon.classList.contains('open')
-
+        const acordeon = trigger.parentElement;
+        const isOpen = acordeon.classList.contains('open');
+        const content = acordeon.querySelector('.content');
         if (isOpen) {
-            acordeon.classList.remove('open')
+            acordeon.classList.remove('open');
+            content.style.height = '0px';
         } else {
-            acordeon.classList.add('open')
+            acordeon.classList.add('open');
+            const contentHeight = content.scrollHeight;
+            content.style.height = `${contentHeight}px`;
         }
-    })
-})
+    });
+});
